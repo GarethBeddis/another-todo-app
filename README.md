25332a490824f247d677555d9ebb15083ef6522d
Gareth Beddis <garethbeddis@gmail.com>
2020-01-16 22:46:42 +0000

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Another Todo App
+
+## Quick Start
+
+```bash
+# Install dependencies
+npm install
+
+# Start development server
+npm run dev
+
+# Build for production
+npm run build
+
+# Start production server
+npm start
+```
---
+++ src/client/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './stylesheets/app.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
